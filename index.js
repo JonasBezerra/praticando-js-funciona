871cc25338c3b5e1eea7ae035bb50adc48d8810d
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-12-15 18:27:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+/* ============== DO WHILE 2020 ============= */
+
+// programação funcional: programação mais declarativa
+
+// MAP, FILTER & REDUCE
+
+/*
+São metodos que permite a gente fazer esse looping por objetos interativo. De forma que usemos valores mutaveis
+
+*/
+
+
+// MAP -> vai passar por cada um desses items do array. vai executar 9 funções como se fossem loopings. e vai retornando os valores dela.
+
+const nums = [1, 4, 6, 5, 7, 7, 8, 9, 10]
+// vamos testar com prog. Estruturada
+
+let newNums_1 = []
+
+for (let n of nums) {
+    newNums_1.push(n * 2)
+}
+
+console.log(newNums_1)
+
+// Vemos que dessa forma temos valores mutaveis, onde num vai mudar a cada interação
+
+// vemos um pouco de programação imperativa onde dizemos como e o q vai ser feito no decorrer do programa.
+// E na programação funcional temos algo mais declarativo, a gente so diz oque vai ser feito. E não como vai ser feito!.
+
+const newNums_2 = nums.map(n => n * 2) // Expressão Lambda
+console.log(newNums_2);
+
+// FILTER -> Passa por cada um dos elementos e retorna somente aquele q corresponde a sua pespectiva
+
+const newNums_3 = nums.filter(n =>{ return n % 2 === 0 })
+console.log(newNums_3)
+
+// REDUCE -> reduzi tudo aquilo a um único valor.
+
+
+
+const soma = nums.reduce((acumulador,num)=> acumulador + num,0)
+console.log(soma)
+// Segundo Parametro é o valor anterior inicial. Um item que nao exite no nosso array. Como assim?.
+// n1 vai somar com o valor inicial que é 0.
+// depois esse valor vai ser acumulado
+
---
+++ spread.js
@@ -0,0 +1,30 @@
+// SPREAD
+const person = {
+     nome: 'Jonas',
+     idade: '19',
+ }
+ 
+ // e se eu quiser colocar mais um atributo ou função nesse objeto. E eu não quero fazer isso manualmente.
+ // quero fazer isso de maneira dinamica no decorrer do meu programa, como faço?
+ 
+ // aplicando o conceito de imutabilidade do paradigma funcional, irá ficar assim
+ 
+ const person2 = {
+     ...person,
+     // eu disse, person2 vai ser igual a tudo que existe em person mais o sobrenome: "vieira"
+     nome: 'Jonas Renan', 
+     // a linha acima é como se fosse uma especie de polimorfismo de sobrePosição, so q nesse caso é sobre o atributo.
+     // pega o atributo anterior do person e sobreEscreve com o atributo novo q esta em person2
+     sobrenome:'Vieira'
+ }
+ 
+ console.log(person2) // { nome: 'Jonas Renan', idade: '19', sobrenome: 'Vieira' }
+ console.log(person) // { nome: 'Jonas', idade: '19' }
+ 
+ // ========= Agora vamos Aplicar em Arrays =======
+ 
+ const numeros = [1,5,4,8,9]
+ const numero2 = [...numeros, 2,3]
+ console.log(numero2) // [1, 5, 4, 8,9, 2, 3]
+ // nesse caso ele não sobreEscreve, so vai colocando os valores do outro array
+ 
